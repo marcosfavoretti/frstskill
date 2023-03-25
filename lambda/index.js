b95d128dc25f82e2f959cdf6e9066d75c1f6eeda
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2023-03-25 19:16:18 +0000

    updated lambda/index.js.

--- lambda/index.js
+++ lambda/index.js
@@ -54,14 +54,18 @@ const nomelist = handlerInput.requestEnvelope.request.intent.slots['nomelist'].v
 axios.get('https://api.trello.com/1/boards/6414eaacdf357282aee076b1/lists?&key=17206af45468d8b12bd543f7f0bb3f86&token=ATTA87f2f270cd37b96abe400dd0bd72a39e50f6f257ef50b9a23c3f0635b6de28ca10C1494B')//req para pegar as lists
     .then(response => {
         const obj = JSON.parse(JSON.stringify(response.data))//json parse => obj recebe json
-        for(var i in obj){
-            if (obj[i].name.toLowerCase() === nomelist){//pegar o valor de todas as listas do quadro
+        for (var i in obj) {
+
+
+            if (obj[i].name.toLowerCase() === nomelist) {//pegar o valor de todas as listas do quadro
                 //ve se tem a lista com o nome passado se tiver vai adicionar o cartao na lista passada
-                axios.post('https://api.trello.com/1/cards?idList='+obj[i].id+'&name='+nome+'&key=17206af45468d8b12bd543f7f0bb3f86&token=ATTA87f2f270cd37b96abe400dd0bd72a39e50f6f257ef50b9a23c3f0635b6de28ca10C1494B');
+                axios.post('https://api.trello.com/1/cards?idList=' + obj[i].id + '&name=' + nome + '&key=17206af45468d8b12bd543f7f0bb3f86&token=ATTA87f2f270cd37b96abe400dd0bd72a39e50f6f257ef50b9a23c3f0635b6de28ca10C1494B');
+                console.log('aki')
                 break//sai do for 
             }
-            else if(nomelist === '' || nomelist === null || nomelist === undefined){//se nao passar nenhuma lista ele vai colocar na primeira lista e sair do for
-                axios.post('https://api.trello.com/1/cards?idList='+obj[i].id+'&name='+nome+'&key=17206af45468d8b12bd543f7f0bb3f86&token=ATTA87f2f270cd37b96abe400dd0bd72a39e50f6f257ef50b9a23c3f0635b6de28ca10C1494B');
+            else if (nomelist === '' || nomelist === null || nomelist === undefined) {//se nao passar nenhuma lista ele vai colocar na primeira lista e sair do for
+                axios.post('https://api.trello.com/1/cards?idList=' + obj[i].id + '&name=' + nome + '&key=17206af45468d8b12bd543f7f0bb3f86&token=ATTA87f2f270cd37b96abe400dd0bd72a39e50f6f257ef50b9a23c3f0635b6de28ca10C1494B');
+                console.log('aki 2')
                 break
             }
         }
