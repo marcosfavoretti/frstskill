ebbf8622c9c1c4770ff1606bd6455afdcf65c967
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2023-03-25 17:39:29 +0000

    updated lambda/index.js.

--- lambda/index.js
+++ lambda/index.js
@@ -53,7 +53,7 @@ const CardCreateIntent = {//funçao para criar cartao
         var speakOutput = axios.post('https://api.trello.com/1/cards?idList=6414eaacdf357282aee076b8&name='+nome+'&key=17206af45468d8b12bd543f7f0bb3f86&token=ATTA87f2f270cd37b96abe400dd0bd72a39e50f6f257ef50b9a23c3f0635b6de28ca10C1494B');
         //speakOutput = nome; 
         return handlerInput.responseBuilder
-            .speak('cartao '+ nome+ 'criado')
+            .speak('cartao '+ nome + ' criado')
             //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
             .getResponse();
     }
