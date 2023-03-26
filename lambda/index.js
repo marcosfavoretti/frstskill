66c9e48495d954aec1886fc3f5c1258e88a579dc
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2023-03-26 00:18:01 +0000

    updated lambda/index.js.

--- lambda/index.js
+++ lambda/index.js
@@ -54,7 +54,7 @@ var due = handlerInput.requestEnvelope.request.intent.slots['dataVencimento'].va
 if(due  === undefined){//trata o valor caso ele nao seja passado
     due = ""
 }
-console.log("->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"+ due)
+console.log("->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"+ due)//saber como vai ser mandado a data na alexa
 axios.get(encodeURI('https://api.trello.com/1/boards/6414eaacdf357282aee076b1/lists?&key=17206af45468d8b12bd543f7f0bb3f86&token=ATTA87f2f270cd37b96abe400dd0bd72a39e50f6f257ef50b9a23c3f0635b6de28ca10C1494B'))//req para pegar as lists
     .then(response => {
         const obj = JSON.parse(JSON.stringify(response.data))//json parse => obj recebe json
@@ -62,11 +62,11 @@ axios.get(encodeURI('https://api.trello.com/1/boards/6414eaacdf357282aee076b1/li
 
             if (obj[i].name.toLowerCase() === nomelist) {//pegar o valor de todas as listas do quadro
                 //ve se tem a lista com o nome passado se tiver vai adicionar o cartao na lista passada
-                axios.post(encodeURI('https://api.trello.com/1/cards?idList=' + obj[i].id + '&name=' + nome + '&key=17206af45468d8b12bd543f7f0bb3f86&token=ATTA87f2f270cd37b96abe400dd0bd72a39e50f6f257ef50b9a23c3f0635b6de28ca10C1494B'));
+                axios.post(encodeURI('https://api.trello.com/1/cards?idList=' + obj[i].id + '&name=' + nome +'&due='+due+'&key=17206af45468d8b12bd543f7f0bb3f86&token=ATTA87f2f270cd37b96abe400dd0bd72a39e50f6f257ef50b9a23c3f0635b6de28ca10C1494B'));
                 break//sai do for 
             }
             else if (nomelist === undefined && nome !== undefined ) {//se nao passar nenhuma lista ele vai colocar na primeira lista e sair do for
-                axios.post( encodeURI('https://api.trello.com/1/cards?idList=' + obj[i].id + '&name=' + nome + '&key=17206af45468d8b12bd543f7f0bb3f86&token=ATTA87f2f270cd37b96abe400dd0bd72a39e50f6f257ef50b9a23c3f0635b6de28ca10C1494B'));
+                axios.post( encodeURI('https://api.trello.com/1/cards?idList=' + obj[i].id + '&name=' + nome + '&due='+due+'&key=17206af45468d8b12bd543f7f0bb3f86&token=ATTA87f2f270cd37b96abe400dd0bd72a39e50f6f257ef50b9a23c3f0635b6de28ca10C1494B'));
                 break
             }
         }
