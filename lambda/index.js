b17238d64f7c841a3fb5cb885c36a9eb264e57af
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2023-05-27 19:54:00 +0000

    updated lambda/index.js.

--- lambda/index.js
+++ lambda/index.js
@@ -147,7 +147,7 @@ const DeleteCardIntent = {
     async handle(handlerInput) {
     const cardname = await handlerInput.requestEnvelope.request.intent.slots['nomeCard'].value
     
-    const urlGetCards =encodeURI( `https://api.trello.com/1/boards/${boardID}/cards?&key=${key}&token${token}`)
+    const urlGetCards =encodeURI( `https://api.trello.com/1/boards/${boardID}/cards?&key=${key}&token=${token}`)
     console.log('----------------------->'+urlGetCards)
     let resp = await axios.get(urlGetCards)
     console.log('antes do resp')
